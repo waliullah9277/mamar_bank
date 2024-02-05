e2821ba76251c69281572ba86b82be20b729d7b6
Waliullah <waliullah9252@gmail.com>
2024-02-05 20:10:12 +0600

    change done

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -34,6 +34,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://mamar-bank-ytur.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
